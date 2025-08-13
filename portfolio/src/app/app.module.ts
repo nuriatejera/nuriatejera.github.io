8b67cdb67649ac2461e27fdcf9dbb4322c9904b0
Nuria Tejera Mirete <ntm18@gcloud.ua.es>
2025-08-14 01:16:15 +0200

    cambiar a component

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing-module';
-import { App } from './app';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    App
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,6 @@ import { App } from './app';
   providers: [
     provideBrowserGlobalErrorListeners()
   ],
-  bootstrap: [App]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
